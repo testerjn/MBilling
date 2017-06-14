5c32c490092e112f1f3aec4e36ffdf02305c54c2
testerjn <testerjn9@gmail.com>
2017-06-14 11:22:48 +0530

    Create Vendor.cs

---
+++ MBilling.Core/Domain/Vendor.cs
@@ -0,0 +1,16 @@
+namespace MBilling.Core.Domain
+{
+    using System;
+    using System.Collections.Generic;
+    using MBilling.Core;
+    
+    public partial class Vendor : Person
+    {
+    
+      public Vendor()
+      {
+
+      }
+    
+    }
+}
