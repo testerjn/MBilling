8d633b099b52b9cb39b89a3adc947e750815df70
testerjn <testerjn9@gmail.com>
2017-06-25 22:31:04 +0530

    Updated changes 25-06-2017 10:30

---
+++ MBilling.Business/Presenters/IPresenter.cs
@@ -0,0 +1,20 @@
+﻿using MBilling.Common.Interfaces;
+using MBilling.Common.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBilling.UI.Presenters
+{
+    public interface IPresenter<Tview, Tmodel>
+      where Tview : IView<Tmodel>
+      where Tmodel : IModel
+    {
+        Tview _view { get; set; }
+        event EventHandler OnViewInitialized;
+        event EventHandler OnViewLoaded;
+    }
+
+}
